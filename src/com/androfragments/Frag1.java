5d25ae01c5126f34c10aa8d22a2a67a60c88aa3b
zoltrax <bernatgrzegorz87@gmail.com>
2013-07-08 15:44:27 +0200

    zmiany zmianeczki

--- src/com/androfragments/Frag1.java
+++ src/com/androfragments/Frag1.java
@@ -16,6 +16,7 @@ import android.view.ViewGroup;
 public class Frag1 extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+		System.out.println("fragment created on greg-corp");
 		return inflater.inflate(R.layout.frag1, container, false);
 	}
 
